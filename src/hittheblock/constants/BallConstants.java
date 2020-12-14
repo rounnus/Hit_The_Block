be323936c4c4a8b17e8bfb8b325f78313560530c
rounnus <rounnus@gmail.com>
2020-12-14 21:42:12 +0200

    Rows and columns and change to enums

--- src/hittheblock/constants/BallConstants.java
+++
@@ -1,8 +0,0 @@
-package hittheblock.constants;
-import javafx.scene.paint.Color;
-
-public class BallConstants {
-    // Ball Specs
-    public static final double BALL_RADIUS = 10;
-    public static final Color BALL_COLOR = Color.BLUE;
-}
--- src/hittheblock/constants/BlockConstants.java
+++
@@ -1,8 +0,0 @@
-package hittheblock.constants;
-
-public class BlockConstants {
-    public static final double BLOCK_HEIGHT = 50;
-    public static final double BLOCK_WEIGHT = 50;
-    public static final double NUMBER_ROWS = 9;
-    public static final double NUMBER_COLUMNS = 4;
-}
